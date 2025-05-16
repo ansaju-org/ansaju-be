744c969b86973e66da3b5e389dc6c496a4397c5d
ikhlashmulya <nurahmanmulya@gmail.com>
2025-05-17 05:26:51 +0700

    change error message

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -1,13 +1,17 @@
 import { z } from "zod";
 import { ResponseError } from "../error/response-error";
 import { injectable } from "tsyringe";
+import { logger } from "../infrastructure/logger";
 
 @injectable()
 export class Validation {
   validate<T extends z.ZodTypeAny>(schema: T, data: unknown) {
     const result = schema.safeParse(data);
     if (!result.success) {
-      throw new ResponseError(400, result.error.message);
+      throw new ResponseError(
+        400,
+        `Invalid request payload: ${result.error.message}`
+      );
     }
     return result.data as z.infer<T>;
   }
